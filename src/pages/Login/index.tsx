ce79aa6897fcc13db20448bf57e04247c99b4d20
arthurssn <arthurnovaes55@gmail.com>
2023-06-01 23:57:35 -0300

    feat: controla spinner e mostra mensagem de erro request

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,5 @@
 import { InputLabel } from "./components/InputLabel";
+
 import React, { useState } from "react";
 import { useAuth } from "../../contexts/AuthContext/useAuth";
 import {
@@ -9,24 +10,43 @@ import {
   HeaderContainer,
   FormGroup,
   FooterContainer,
+  ContainerErrorMessage,
 } from "./styles";
 import { IUserAuth } from "../../interfaces/IUserAuth";
 import { AppButton } from "./components/AppButton";
+import { AxiosError } from "axios";
+
+interface ApiResponse {
+  error: string;
+}
 
 export default function Login() {
-  const authContext = useAuth();
-
   const [user, setUser] = useState<IUserAuth>({ email: "", password: "" });
+  const [sendingData, setSendingData] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>("");
 
-  function login(e: React.FormEvent) {
+  const authContext = useAuth();
+  async function login(e: React.FormEvent) {
     e.preventDefault();
-    authContext.login(user);
+    setSendingData(true);
+    try {
+      await authContext.login(user);
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      const errorMessage = axiosError.response?.data?.error;
+      setErrorMessage(errorMessage);
+    } finally {
+      setSendingData(false);
+    }
   }
   return (
     <ContainerPageLogin>
       <ContainerFormLogin onSubmit={login}>
         <HeaderContainer>
           <LoginTitle>GoalManagement</LoginTitle>
+          {errorMessage && (
+            <ContainerErrorMessage>{errorMessage}</ContainerErrorMessage>
+          )}
         </HeaderContainer>
 
         <FormGroup>
@@ -44,13 +64,18 @@ export default function Login() {
               type="password"
               icon="src/assets/icons/lock_icon.svg"
               placeholder="Password"
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
             />
           </InputGroup>
         </FormGroup>
 
         <FooterContainer>
-          <AppButton text="Login" backgroundColor="#ff3f34" color="#fff" />
+          <AppButton
+            text="Login"
+            backgroundColor="#ff3f34"
+            color="#fff"
+            loading={sendingData}
+          />
           <p
             style={{
               color: "rgba(102, 102, 102, 0.75)",
