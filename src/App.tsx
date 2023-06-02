b5abf00775e9697061fc90eee2812bea8a925b7f
arthurssn <arthurnovaes55@gmail.com>
2023-06-02 14:47:14 -0300

    feat: add theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { AuthProvider } from "./contexts/AuthContext";
+import { CustomThemeProvider } from "./contexts/ThemeContext";
 import Login from "./pages/Login";
 import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <AuthProvider>
-        <Login></Login>
-      </AuthProvider>
+      <CustomThemeProvider>
+        <GlobalStyle />
+        <AuthProvider>
+          <Login></Login>
+        </AuthProvider>
+      </CustomThemeProvider>
     </>
   );
 }
