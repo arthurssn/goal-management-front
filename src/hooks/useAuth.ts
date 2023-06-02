79474090684773e95f2566f77095cbf825b839bb
arthurssn <arthurnovaes55@gmail.com>
2023-06-02 14:43:59 -0300

    feat: cria pasta de hooks e move hooks para ela

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from ".";
+import { AuthContext } from "@/contexts/AuthContext";
 
 export const useAuth = () => {
   return useContext(AuthContext);
