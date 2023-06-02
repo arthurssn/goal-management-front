c4389bdbe58bc6d058b79682c367b164970d8822
arthurssn <arthurnovaes55@gmail.com>
2023-06-02 14:47:04 -0300

    feat: inicia default theme

---
+++ src/themes/defaultTheme.ts
@@ -0,0 +1,12 @@
+import { Theme } from "@/contexts/ThemeContext/interfaces";
+
+export const defaultTheme: Theme = {
+  name: "defaultTheme",
+  colors: {
+    red: "#ff3f34",
+    white: "#fff",
+  },
+  alertsColor: {
+    warning: "rgba(255, 162, 0, 0.75)",
+  },
+};
