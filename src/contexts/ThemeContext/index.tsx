c490321cc48f0c47656ee2b464fa3b9deee7603a
arthurssn <arthurnovaes55@gmail.com>
2023-06-03 16:10:04 -0300

    fix: corrige importacao

--- src/contexts/ThemeContext/index.tsx
+++ src/contexts/ThemeContext/index.tsx
@@ -1,10 +1,11 @@
 import { createContext, useState } from "react";
 import { ThemeProvider } from "styled-components";
 
-import { Theme, ThemeContextData } from "./interfaces";
+import { ThemeContextData } from "./interfaces";
 import { IContextProvider } from "@/interfaces/IContextProvider";
 
 import { defaultTheme } from "@/themes/defaultTheme";
+import { Theme } from "@/interfaces/ITheme";
 
 export const ThemeContext = createContext<ThemeContextData>(
   {} as ThemeContextData
@@ -13,7 +14,7 @@ export const ThemeContext = createContext<ThemeContextData>(
 export const CustomThemeProvider = ({ children }: IContextProvider) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
 
-  function toggleTheme(name: "defaultTheme" | "darkTheme") {
+  function toggleTheme(name: "defaultTheme") {
     if (!name || name == "defaultTheme") {
       setTheme(defaultTheme);
       return;
