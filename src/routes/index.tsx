2f6145344fa6d564752f21b2360336071a58ba32
arthurssn <arthurnovaes55@gmail.com>
2023-06-13 21:17:51 -0300

    feat: protecao de rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "@/pages/Auth/Login";
 import Register from "@/pages/Auth/Register";
 import Home from "@/pages/Home";
 import DashboardTemplate from "@/components/template/DashboardTemplate";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import { useAuth } from "@/hooks/useAuth";
 
 const AppRoutes = () => {
+  const { email, isInitialized } = useAuth();
+  if (!isInitialized) return;
+
+  const emailExists = email !== undefined;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={emailExists ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={emailExists ? <Navigate to="/" /> : <Register />}
+        />
         <Route element={<DashboardTemplate />}>
-          <Route path="/" element={<Home />} />
+          <Route element={<ProtectedRoute isAllowed={emailExists} />}>
+            <Route path="/" element={<Home />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
