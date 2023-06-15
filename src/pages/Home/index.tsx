a9b7fec951efe5ac5821afc9317d6d1418ee203b
arthurssn <arthurnovaes55@gmail.com>
2023-06-15 17:29:35 -0300

    feat: busca itens e modifica status

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import AppSelect from "@/components/forms/AppSelect";
 import { InputWithBorderBottom } from "@/components/forms/InputWithBorderBottom";
 import { IAppSelectOptions } from "@/interfaces/forms/IAppSelectOptions";
 import search_icon from "@/assets/icons/search_icon.svg";
-import { ChangeEvent } from "react";
+import { useEffect, useState } from "react";
 import {
   CreateButtonContainer,
   InputsFilterContainer,
@@ -11,6 +11,9 @@ import {
 import IndexList from "@/components/layout/IndexList";
 import CreateButton from "./components/CreateButton";
 import ItemList from "./components/ItemList";
+import { getAll } from "@/services/GoalsService";
+import { IGoal } from "@/interfaces/IGoal";
+import { GoalStatus } from "@/enums/GoalStatus";
 export default function Home() {
   const options: IAppSelectOptions[] = [
     { id: 1, label: "Daily", value: 1 },
@@ -19,9 +22,25 @@ export default function Home() {
     { id: 4, label: "Annually", value: 4 },
   ];
 
-  function handleSearchItem(e: ChangeEvent<HTMLInputElement>) {
-    console.log(e);
+  const [goals, setGoals] = useState<IGoal[]>([]);
+
+  async function getGoals() {
+    const response = await getAll();
+    setGoals(response);
   }
+
+  function updateGoalStatus(id: number, status: GoalStatus) {
+    setGoals((prevGoals) => {
+      return prevGoals.map((goal) => ({
+        ...goal,
+        status: goal.id === id ? status : goal.status,
+      }));
+    });
+  }
+
+  useEffect(() => {
+    getGoals();
+  }, []);
 
   return (
     <IndexList
@@ -34,18 +53,24 @@ export default function Home() {
               name="deadlineSelect"
               label="Select"
               options={options}
+              value={undefined}
             />
             <InputWithBorderBottom
               icon={search_icon}
               placeholder="Search Items"
-              onChange={handleSearchItem}
             />
           </InputsFilterContainer>
           <CreateButtonContainer>
             <CreateButton />
           </CreateButtonContainer>
           <ListContainer>
-            <ItemList />
+            {goals.map((goal) => (
+              <ItemList
+                goal={goal}
+                onUpdateStatus={updateGoalStatus}
+                key={goal.id}
+              />
+            ))}
           </ListContainer>
         </>
       }
