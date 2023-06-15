da2577c4d4cbb1490829a0d7887d6628d21a9401
arthurssn <arthurnovaes55@gmail.com>
2023-06-15 10:03:00 -0300

    feat: inicia chechkbox component

---
+++ src/components/forms/AppCheckbox/index.tsx
@@ -0,0 +1,5 @@
+import { CheckboxItem } from "./styles";
+
+export default function AppCheckbox() {
+  return <CheckboxItem type="checkbox" />;
+}
---
+++ src/components/forms/AppCheckbox/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CheckboxItem = styled.input.attrs({ type: "checkbox" })`
+  background: red;
+  border: 1px solid #ff3f34;
+  border-radius: 5px;
+`;
