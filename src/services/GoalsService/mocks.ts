003d29d245b90573763c6d208aabb94757fea2f7
arthurssn <arthurnovaes55@gmail.com>
2023-06-15 17:27:33 -0300

    feat: add itens ao mock

--- src/services/GoalsService/mocks.ts
+++ src/services/GoalsService/mocks.ts
@@ -1,13 +1,37 @@
 import { GoalStatus } from "@/enums/GoalStatus";
 import { IGoal } from "@/interfaces/IGoal";
 
+var dataAtual = new Date();
+dataAtual.setFullYear(dataAtual.getFullYear() + 1);
+
 export const goals: IGoal[] = [
   {
-    id: 13289,
+    id: 1,
     title: "Study english for 45 minutes",
     status: GoalStatus.Pending,
     description:
       "Study english for 45 minutes, Study english for 45 minutes, Study english for 45 minutes, Study english for 45 minutes, Study english for 45 minutes",
+    deadline: new Date(),
+  },
+  {
+    id: 2,
+    title: "Acc 10k",
+    status: GoalStatus.InProgress,
+    description: "Acc 10k",
+    deadline: new Date(),
+  },
+  {
+    id: 3,
+    title: "Read 24 books",
+    status: GoalStatus.InProgress,
+    description: "Read 24 books",
+    deadline: new Date(),
+  },
+  {
+    id: 4,
+    title: "Start learn english",
+    status: GoalStatus.Done,
+    description: "Buy an English course and get started",
     deadline: new Date(),
   },
 ];
