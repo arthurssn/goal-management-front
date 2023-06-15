1d405191f105588dcd53ca5e1e4f01fe8ec03c0c
arthurssn <arthurnovaes55@gmail.com>
2023-06-15 10:03:49 -0300

    feat: comeca estrutura da home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,54 @@
+import AppSelect from "@/components/forms/AppSelect";
+import { InputWithBorderBottom } from "@/components/forms/InputWithBorderBottom";
+import { IAppSelectOptions } from "@/interfaces/forms/IAppSelectOptions";
+import search_icon from "@/assets/icons/search_icon.svg";
+import { ChangeEvent } from "react";
+import {
+  CreateButtonContainer,
+  InputsFilterContainer,
+  ListContainer,
+} from "./styles";
+import IndexList from "@/components/layout/IndexList";
+import CreateButton from "./components/CreateButton";
+import ItemList from "./components/ItemList";
 export default function Home() {
+  const options: IAppSelectOptions[] = [
+    { id: 1, label: "Daily", value: 1 },
+    { id: 2, label: "Weekly", value: 2 },
+    { id: 3, label: "Monthly", value: 3 },
+    { id: 4, label: "Annually", value: 4 },
+  ];
+
+  function handleSearchItem(e: ChangeEvent<HTMLInputElement>) {
+    console.log(e);
+  }
+
   return (
-    <div>
-      <h1 style={{ color: "black" }}>:)</h1>
-    </div>
+    <IndexList
+      title="Goals"
+      content={
+        <>
+          <InputsFilterContainer>
+            <AppSelect
+              id="deadlineSelect"
+              name="deadlineSelect"
+              label="Select"
+              options={options}
+            />
+            <InputWithBorderBottom
+              icon={search_icon}
+              placeholder="Search Items"
+              onChange={handleSearchItem}
+            />
+          </InputsFilterContainer>
+          <CreateButtonContainer>
+            <CreateButton />
+          </CreateButtonContainer>
+          <ListContainer>
+            <ItemList />
+          </ListContainer>
+        </>
+      }
+    />
   );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,24 @@
+import { devices } from "@/constants/devices";
+import styled from "styled-components";
+
+export const InputsFilterContainer = styled.div`
+  display: flex;
+  gap: 30px;
+  * {
+    height: 35px;
+  }
+  flex-direction: column;
+  @media ${devices.tablet} {
+    flex-direction: row;
+  }
+`;
+
+export const CreateButtonContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: end;
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 20px;
+`;
