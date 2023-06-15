ce461e0f4cfe59f6c4ff48f545ee92431b081a99
arthurssn <arthurnovaes55@gmail.com>
2023-06-15 17:27:56 -0300

    feat: componente de item renderizado

--- src/pages/Home/components/ItemList/index.tsx
+++ src/pages/Home/components/ItemList/index.tsx
@@ -1,19 +1,74 @@
-import AppCheckbox from "@/components/forms/AppCheckbox";
-import { ItemContent, ItemListContainer, Separator } from "./styles";
+import {
+  DescriptionText,
+  ExpandButton,
+  ItemContent,
+  ItemListContainer,
+  Separator,
+  StatusSelectContainer,
+  StatusText,
+  TitleText,
+} from "./styles";
 import chevron from "./chevron.svg";
-export default function ItemList() {
+import { IGoal } from "@/interfaces/IGoal";
+import {
+  getGoalStatusColor,
+  getGoalStatusText,
+  reverseGoalEnum,
+} from "@/utils/goalUtils";
+import { ChangeEvent, useState } from "react";
+import { updateStatus } from "@/services/GoalsService";
+import { GoalStatus } from "@/enums/GoalStatus";
+import AppSelect from "@/components/forms/AppSelect";
+
+export default function ItemList({
+  goal,
+  onUpdateStatus,
+}: {
+  goal: IGoal;
+  onUpdateStatus: (id: number, status: GoalStatus) => void;
+}) {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const statusOptions = [
+    { id: 3, label: "Done", value: 1 },
+    { id: 1, label: "Pending", value: 2 },
+    { id: 2, label: "In Progress", value: 3 },
+  ];
+  function toggleExpanded() {
+    setExpanded((prev) => !prev);
+  }
+
+  function tryUpdateGoalStatus(e: ChangeEvent<HTMLSelectElement>) {
+    const status = reverseGoalEnum(Number(e.target.value));
+    updateStatus(goal.id, status).then(() => {
+      onUpdateStatus(goal.id, status);
+    });
+  }
+
   return (
     <ItemListContainer>
       <ItemContent>
-        <span>
-          <AppCheckbox /> Status: PendinStudy english for 45 minutes
-        </span>
-        <span>Status: Pending</span>
-        <span>Professional</span>
-        <span>
+        <TitleText>
+          <StatusSelectContainer>
+            <AppSelect
+              id="status"
+              label="Select"
+              name="status"
+              options={statusOptions}
+              onChange={tryUpdateGoalStatus}
+              value={goal.status}
+            />
+          </StatusSelectContainer>
+          {goal.title}
+        </TitleText>
+        <StatusText color={getGoalStatusColor(goal.status)}>
+          Status: {getGoalStatusText(goal.status)}
+        </StatusText>
+        <span>Deadline: {goal.deadline.toLocaleDateString()} </span>
+        <ExpandButton onClick={toggleExpanded}>
           <img src={chevron} />
-        </span>
+        </ExpandButton>
       </ItemContent>
+      {expanded && <DescriptionText>{goal.description}</DescriptionText>}
       <Separator />
     </ItemListContainer>
   );
--- src/pages/Home/components/ItemList/styles.ts
+++ src/pages/Home/components/ItemList/styles.ts
@@ -1,3 +1,4 @@
+import { devices } from "@/constants/devices";
 import styled from "styled-components";
 
 export const ItemListContainer = styled.div`
@@ -15,16 +16,58 @@ export const Separator = styled.div`
 `;
 
 export const ItemContent = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-
   * {
     font-style: normal;
     font-weight: 500;
     font-size: 16px;
     line-height: 20px;
+  }
+  display: grid;
+  width: 100%;
+  grid-column-gap: 50px;
+  grid-row-gap: 15px;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  @media ${devices.tablet} {
+    grid-template-columns: 1fr 150px 220px 24px;
+    grid-template-rows: 1fr;
+  }
+`;
+
+export const StatusText = styled.p<{ color: string }>`
+  color: ${(props) => props.theme.colors[props.color]};
+`;
+
+export const TitleText = styled.span`
+  cursor: pointer;
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  display: flex;
+  gap: 10px;
+`;
+
+export const ExpandButton = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  @media ${devices.tablet} {
+    display: block;
+  }
+`;
+
+export const DescriptionText = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export const StatusSelectContainer = styled.div`
+  width: 100%;
+  @media ${devices.tablet} {
+    width: 200px;
   }
 `;
