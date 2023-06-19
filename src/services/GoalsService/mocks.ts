542a484ff5d11e19e47682e2e5033cc62d4f8458
arthurssn <arthurnovaes55@gmail.com>
2023-06-19 16:03:26 -0300

    refactor: modifica lista de mocks

--- src/services/GoalsService/mocks.ts
+++ src/services/GoalsService/mocks.ts
@@ -10,53 +10,24 @@ inThisYear.setMonth(inThisMonth.getMonth() + 5);
 export const goals: IGoal[] = [
   {
     id: 1,
-    title: "Study english for 45 minutes",
-    status: GoalStatus.Pending,
-    description:
-      "Study english for 45 minutes, Study english for 45 minutes, Study english for 45 minutes, Study english for 45 minutes, Study english for 45 minutes",
+    title: "Meta Diária",
     deadline: new Date(),
+    status: GoalStatus.Done,
   },
   {
     id: 2,
-    title: "Acc 10k",
-    status: GoalStatus.InProgress,
-    description: "Acc 10k",
-    deadline: new Date(),
-  },
-  {
-    id: 3,
-    title: "Read 24 books",
-    status: GoalStatus.InProgress,
-    description: "Read 24 books",
-    deadline: new Date(),
-  },
-  {
-    id: 4,
-    title: "start learning english",
-    status: GoalStatus.Done,
-    description: "Buy an English course and get started",
-    deadline: new Date(),
-  },
-  {
-    id: 5,
-    title: "Meta Diária",
-    deadline: new Date(),
-    status: GoalStatus.InProgress,
-  },
-  {
-    id: 6,
     title: "Meta Semanal",
     deadline: onNextWeek,
     status: GoalStatus.Done,
   },
   {
-    id: 7,
+    id: 3,
     title: "Meta Mensal",
     deadline: inThisMonth,
     status: GoalStatus.Pending,
   },
   {
-    id: 8,
+    id: 4,
     title: "Meta Anual",
     deadline: inThisYear,
     status: GoalStatus.InProgress,
