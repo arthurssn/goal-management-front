74db56a28b03bd92bbc743ff30a11707ea0d2d52
arthurssn <arthurnovaes55@gmail.com>
2023-06-23 15:40:04 -0300

    fix: corrige erro na importacao de arquivos

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,14 +1,15 @@
 import { CircleUserProfilePic, HeaderContainer, UserInfo } from "./styles";
-
+import user_icon from "@/assets/icons/user_icon.svg";
+import home_icon from "@/assets/icons/home_icon.svg";
 export default function Header() {
   return (
     <HeaderContainer>
-      <img src="src/assets/icons/home_icon.svg" alt="home_icon" />
+      <img src={home_icon} alt="home_icon" />
 
       <UserInfo>
         Bem-vindo!
         <CircleUserProfilePic>
-          <img src="src/assets/icons/user_icon.svg" alt="user_icon" />
+          <img src={user_icon} alt="user_icon" />
         </CircleUserProfilePic>
       </UserInfo>
     </HeaderContainer>
