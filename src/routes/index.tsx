0c2b4d309760e9dfada0b3d71bb686515d4ddef4
arthurssn <arthurnovaes55@gmail.com>
2023-06-23 17:45:47 -0300

    feat: add suspense e lazy load

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
-import Login from "@/pages/Auth/Login";
-import Register from "@/pages/Auth/Register";
-import Home from "@/pages/Home";
 import DashboardTemplate from "@/components/template/DashboardTemplate";
 import ProtectedRoute from "@/components/ProtectedRoute";
+
 import { useAuth } from "@/hooks/useAuth";
+
+const Login = lazy(() => import("@/pages/Auth/Login"));
+const Register = lazy(() => import("@/pages/Auth/Register"));
+const Home = lazy(() => import("@/pages/Home"));
+const CreateGoal = lazy(() => import("@/pages/Goals/CreateGoal"));
 
 const AppRoutes = (): JSX.Element | null => {
   const { email, isInitialized } = useAuth();
@@ -14,21 +18,24 @@ const AppRoutes = (): JSX.Element | null => {
   const emailExists = email !== undefined;
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/login"
-          element={emailExists ? <Navigate to="/" /> : <Login />}
-        />
-        <Route
-          path="/register"
-          element={emailExists ? <Navigate to="/" /> : <Register />}
-        />
-        <Route element={<DashboardTemplate />}>
-          <Route element={<ProtectedRoute isAllowed={emailExists} />}>
-            <Route path="/" element={<Home />} />
+      <Suspense>
+        <Routes>
+          <Route
+            path="/login"
+            element={emailExists ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/register"
+            element={emailExists ? <Navigate to="/" /> : <Register />}
+          />
+          <Route element={<DashboardTemplate />}>
+            <Route element={<ProtectedRoute isAllowed={emailExists} />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/goals/create" element={<CreateGoal />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
